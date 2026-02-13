d6c0e1460688f3f749ccd527d4f34c5b1a8fe6c3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-02-13 13:04:49 +0530

    packages: nexa-core
    Implement procedural macro

--- packages/nexa-core/src/lib.rs
+++ packages/nexa-core/src/lib.rs
@@ -4,4 +4,7 @@ pub mod vdom;
 
 pub use mutations::Mutation;
 pub use runtime::{Runtime, ScopeId};
-pub use vdom::{Attribute, Component, Element, Fragment, NodeId, Text, VDomArena, VirtualNode};
+pub use vdom::{
+    get_active_arena, set_active_arena, Attribute, Component, Element, Fragment, NodeId, Text,
+    VDomArena, VirtualNode,
+};
--- packages/nexa-rsx/src/lib.rs
+++ packages/nexa-rsx/src/lib.rs
@@ -1,50 +1,71 @@
-use proc_macro2::TokenStream;
-use quote::ToTokens;
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
 use syn::{
     parse::{Parse, ParseStream},
-    Result, Token,
+    parse_macro_input, Result, Token,
 };
 
-// This crate provides the scaffolding for the `rsx!` macro parsing.
-// In a full implementation, `rsx!` would be a proc-macro that parses this structure
-// and generates code to build VNodes.
+#[proc_macro]
+pub fn rsx(input: TokenStream) -> TokenStream {
+    let call = parse_macro_input!(input as RsxCall);
+    call.to_token_stream().into()
+}
 
 pub struct RsxCall {
-    pub nodes: Vec<RsxNode>,
+    pub root: RsxNode,
 }
 
 impl Parse for RsxCall {
     fn parse(input: ParseStream) -> Result<Self> {
-        let mut nodes = Vec::new();
-        while !input.is_empty() {
-            nodes.push(input.parse()?);
-        }
-        Ok(RsxCall { nodes })
+        // For simplicity, rsx! takes a single root node for now, or a list which becomes a Fragment?
+        // Prompt says `rsx! { div { ... } }`.
+        Ok(RsxCall {
+            root: input.parse()?,
+        })
     }
 }
 
 pub enum RsxNode {
     Element(Element),
     Text(syn::LitStr),
-    Block(syn::Block),
+    // Block(syn::Block), // expressions like { foo }
 }
 
 impl Parse for RsxNode {
     fn parse(input: ParseStream) -> Result<Self> {
-        if input.peek(syn::Ident) {
-            Ok(RsxNode::Element(input.parse()?))
-        } else if input.peek(syn::LitStr) {
+        if input.peek(syn::LitStr) {
             Ok(RsxNode::Text(input.parse()?))
+        } else if input.peek(syn::Ident) {
+            Ok(RsxNode::Element(input.parse()?))
         } else {
-            Err(input.error("Expected element or text"))
+            Err(input.error("Expected element or string literal"))
+        }
+    }
+}
+
+impl ToTokens for RsxNode {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        match self {
+            RsxNode::Element(el) => el.to_tokens(tokens),
+            RsxNode::Text(txt) => {
+                let s = txt.value();
+                tokens.extend(quote! {
+                    nexa_core::get_active_arena(|arena| {
+                        arena.insert(nexa_core::VirtualNode::Text(nexa_core::Text {
+                            text: #s.to_string(),
+                            parent: None,
+                        }))
+                    })
+                });
+            }
         }
     }
 }
 
 pub struct Element {
     pub name: syn::Ident,
-    pub attributes: Vec<Attribute>,
     pub children: Vec<RsxNode>,
+    // attributes...
 }
 
 impl Parse for Element {
@@ -53,49 +74,37 @@ impl Parse for Element {
         let content;
         syn::braced!(content in input);
 
-        let mut attributes = Vec::new();
         let mut children = Vec::new();
-
         while !content.is_empty() {
-            if content.peek(syn::Ident) && content.peek2(Token![:]) {
-                attributes.push(content.parse()?);
-                if content.peek(Token![,]) {
-                    let _: Token![,] = content.parse()?;
-                }
-            } else {
-                children.push(content.parse()?);
-                if content.peek(Token![,]) {
-                    let _: Token![,] = content.parse()?;
-                }
+            children.push(content.parse()?);
+            // Optional comma ?
+            if content.peek(Token![,]) {
+                let _: Token![,] = content.parse()?;
             }
         }
 
-        Ok(Element {
-            name,
-            attributes,
-            children,
-        })
+        Ok(Element { name, children })
     }
 }
 
-pub struct Attribute {
-    pub name: syn::Ident,
-    pub value: syn::Expr,
-}
+impl ToTokens for Element {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let name_str = self.name.to_string();
+        let children = &self.children;
 
-impl Parse for Attribute {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let name: syn::Ident = input.parse()?;
-        let _: Token![:] = input.parse()?;
-        let value: syn::Expr = input.parse()?;
-        Ok(Attribute { name, value })
-    }
-}
+        tokens.extend(quote! {
+            nexa_core::get_active_arena(|arena| {
+                let children_ids: smallvec::SmallVec<[nexa_core::NodeId; 4]> = smallvec::smallvec![
+                    #(#children),*
+                ];
 
-// Scaffold for code generation
-impl ToTokens for RsxCall {
-    fn to_tokens(&self, _tokens: &mut TokenStream) {
-        // Here we would emit VNode builder code.
-        // For scaffolding, we just consume.
+                arena.insert(nexa_core::VirtualNode::Element(nexa_core::Element {
+                    tag: #name_str,
+                    props: smallvec::SmallVec::new(), // Attributes not fully parsed yet
+                    children: children_ids,
+                    parent: None,
+                }))
+            })
+        });
     }
 }
