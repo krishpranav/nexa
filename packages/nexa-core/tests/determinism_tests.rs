c5ba8c6cb3d62bc37dbc74e3317b271234b9dc2a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-02-14 16:37:23 +0530

    tests: nexa-core[determinism tests]

---
+++ packages/nexa-core/tests/determinism_tests.rs
@@ -0,0 +1,24 @@
+use nexa_core::*;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+#[test]
+fn test_deterministic_execution_order() {
+    let results = Arc::new(Mutex::new(Vec::new()));
+
+    for i in 0..10 {
+        let results = results.clone();
+        let handle = thread::spawn(move || {
+            // Simulate some work
+            thread::sleep(std::time::Duration::from_millis(10 - i));
+            results.lock().unwrap().push(i);
+        });
+        let _ = handle.join();
+    }
+
+    let res = results.lock().unwrap();
+    // This should be 0, 1, 2... because we join them sequentially
+    for (idx, &val) in res.iter().enumerate() {
+        assert_eq!(idx, val as usize);
+    }
+}
