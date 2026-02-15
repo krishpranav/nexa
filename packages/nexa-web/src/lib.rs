2d2c82fb4cbb60e086b8901980a09f63dc829507
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-02-15 18:26:09 +0530

    packages: nexa-web

--- packages/nexa-web/src/lib.rs
+++ packages/nexa-web/src/lib.rs
@@ -16,28 +16,35 @@ struct WebInterpreter {
     nodes: HashMap<u64, Node>,
     event_listeners: HashMap<u64, Vec<Closure<dyn FnMut(Event)>>>,
     root_id: Option<u64>,
+    runtime: Rc<RefCell<Runtime>>,
 }
 
 impl WebInterpreter {
-    fn new(document: Document) -> Self {
+    fn new(document: Document, runtime: Rc<RefCell<Runtime>>) -> Self {
         Self {
             document,
             nodes: HashMap::new(),
             event_listeners: HashMap::new(),
             root_id: None,
+            runtime,
         }
     }
 
-    fn apply_mutations(&mut self, mutations: Vec<Mutation>) {
-        // Efficient batching: We use a single loop to apply all mutations.
-        // In a more advanced implementation, we could sort or group them.
+    fn apply_mutations(&mut self, mutations: Vec<Mutation>, handle: Rc<RefCell<WebInterpreter>>) {
+        tracing::debug!("Applying {} mutations", mutations.len());
         for mutation in mutations {
+            tracing::trace!("Mutation: {:?}", mutation);
             match mutation {
                 Mutation::PushRoot { id } => {
                     self.root_id = Some(id);
+                    tracing::debug!("Root ID set to {}", id);
                 }
                 Mutation::CreateElement { tag, id } => {
+                    web_sys::console::log_1(
+                        &format!("Created element '{}' with id {}", tag, id).into(),
+                    );
                     let el = self.document.create_element(&tag).unwrap();
+                    el.set_attribute("data-nexa-id", &id.to_string()).unwrap();
                     self.nodes.insert(id, el.into());
                 }
                 Mutation::CreateTextNode { text, id } => {
@@ -45,10 +52,19 @@ impl WebInterpreter {
                     self.nodes.insert(id, node.into());
                 }
                 Mutation::AppendChildren { id, m } => {
-                    let parent = self.nodes.get(&id).unwrap();
+                    let parent = if id == 0 {
+                        // Special case for container
+                        // In mount, we inserted container as 0
+                        self.nodes.get(&0).expect("Container not found (id=0)")
+                    } else {
+                        self.nodes.get(&id).expect("Parent node not found")
+                    };
+
                     for child_id in m {
                         if let Some(child) = self.nodes.get(&child_id) {
                             parent.append_child(child).unwrap();
+                        } else {
+                            tracing::error!("Child node {} not found for append", child_id);
                         }
                     }
                 }
@@ -67,7 +83,7 @@ impl WebInterpreter {
                     }
                 }
                 Mutation::NewEventListener { name, id } => {
-                    self.add_event_listener(id, &name);
+                    self.add_event_listener(id, &name, handle.clone());
                 }
                 Mutation::Remove { id } => {
                     if let Some(node) = self.nodes.remove(&id) {
@@ -84,26 +100,54 @@ impl WebInterpreter {
         }
     }
 
-    fn add_event_listener(&mut self, id: u64, event_name: &str) {
-        let node = self.nodes.get(&id).unwrap().clone();
+    fn add_event_listener(
+        &mut self,
+        id: u64,
+        event_name: &str,
+        handle: Rc<RefCell<WebInterpreter>>,
+    ) {
+        web_sys::console::log_1(&format!("Adding listener '{}' to node {}", event_name, id).into());
+        let node = if let Some(n) = self.nodes.get(&id) {
+            n.clone()
+        } else {
+            tracing::error!("Cannot add listener to missing node {}", id);
+            return;
+        };
+
+        // Clone runtime for the closure
+        let runtime = self.runtime.clone();
+        let name = event_name.to_string();
+        let node_id = id;
+
+        // Clone handle for the closure
+        let interpreter_handle = handle;
 
         let closure = Closure::wrap(Box::new(move |event: Event| {
-            if let Some(target) = event.target() {
-                if let Some(input) = target.dyn_ref::<web_sys::HtmlInputElement>() {
-                    if input.type_() == "file" {
-                        if let Some(files) = input.files() {
-                            for i in 0..files.length() {
-                                if let Some(file) = files.get(i) {
-                                    web_sys::console::log_1(
-                                        &format!("File selected: {}", file.name()).into(),
-                                    );
-                                }
-                            }
-                        }
-                    }
+            // Map web_sys Event to nexa_core Event
+            let nexa_event = match event.type_().as_str() {
+                "click" => nexa_core::Event::Click,
+                "input" => {
+                    let value = event
+                        .target()
+                        .and_then(|t| t.dyn_into::<web_sys::HtmlInputElement>().ok())
+                        .map(|input| input.value())
+                        .unwrap_or_default();
+                    nexa_core::Event::Input(value)
                 }
+                _ => nexa_core::Event::Unknown,
+            };
+
+            runtime
+                .borrow_mut()
+                .handle_event(node_id, &name, nexa_event);
+
+            // Trigger updates
+            let mutations = runtime.borrow_mut().drain_mutations();
+            if !mutations.is_empty() {
+                interpreter_handle
+                    .borrow_mut()
+                    .apply_mutations(mutations, interpreter_handle.clone());
             }
-            web_sys::console::log_1(&format!("Event triggered on node {}", id).into());
         }) as Box<dyn FnMut(Event)>);
 
         node.add_event_listener_with_callback(event_name, closure.as_ref().unchecked_ref())
@@ -123,9 +167,12 @@ impl WebApp {
         let window = web_sys::window().unwrap();
         let document = window.document().unwrap();
 
+        let runtime = Rc::new(RefCell::new(Runtime::new()));
+        let interpreter = Rc::new(RefCell::new(WebInterpreter::new(document, runtime.clone())));
+
         let app = Self {
-            runtime: Rc::new(RefCell::new(Runtime::new())),
-            interpreter: Rc::new(RefCell::new(WebInterpreter::new(document))),
+            runtime,
+            interpreter,
         };
 
         // Devtools hook injection
@@ -140,23 +187,6 @@ impl WebApp {
         let _ = js_sys::Reflect::set(&window, &"__NEXA_DEVTOOLS__".into(), &JsValue::TRUE);
     }
 
-    pub fn mount(&mut self, root_id: &str) -> Result<(), JsValue> {
-        let window = web_sys::window().unwrap();
-        let document = window.document().unwrap();
-        let root_el = document
-            .get_element_by_id(root_id)
-            .ok_or_else(|| JsValue::from_str("Root element not found"))?;
-
-        // Initialize root mapping
-        self.interpreter
-            .borrow_mut()
-            .nodes
-            .insert(0, root_el.into());
-
-        self.update()?;
-        Ok(())
-    }
-
     pub fn update(&mut self) -> Result<(), JsValue> {
         let window = web_sys::window().unwrap();
         let performance = window.performance().unwrap();
@@ -165,7 +195,10 @@ impl WebApp {
         self.runtime.borrow_mut().update();
         let mutations = self.runtime.borrow_mut().drain_mutations();
 
-        self.interpreter.borrow_mut().apply_mutations(mutations);
+        let interpreter_clone = self.interpreter.clone();
+        self.interpreter
+            .borrow_mut()
+            .apply_mutations(mutations, interpreter_clone);
         let end = performance.now();
 
         // Performance instrumentation
@@ -222,5 +255,31 @@ impl WebApp {
 
         let _ = promise.then(&closure);
         closure.forget();
+    }
+}
+
+impl WebApp {
+    pub fn mount(
+        &mut self,
+        root_id: &str,
+        root_fn: fn() -> nexa_core::NodeId,
+    ) -> Result<(), JsValue> {
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        let id = root_id.strip_prefix('#').unwrap_or(root_id);
+        let root_el = document
+            .get_element_by_id(id)
+            .ok_or_else(|| JsValue::from_str(&format!("Root element not found: {}", id)))?;
+
+        // Initialize root mapping
+        self.interpreter
+            .borrow_mut()
+            .nodes
+            .insert(0, root_el.into());
+
+        self.runtime.borrow_mut().mount("Root", root_fn);
+
+        self.update()?;
+        Ok(())
     }
 }
