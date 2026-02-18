236d717b80bf77a6d0e29ea925de2375ad94af20
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-02-18 23:10:50 +0530

    packages: nexa-signals

--- packages/nexa-signals/src/lib.rs
+++ packages/nexa-signals/src/lib.rs
@@ -5,3 +5,5 @@ pub mod signal;
 pub use graph::{Graph, NodeType, SignalId};
 pub use signal::Memo as Computed;
 pub use signal::{Effect, Memo, Signal, create_effect, create_memo, signal};
+pub mod scheduler;
+pub use scheduler::Scheduler;
